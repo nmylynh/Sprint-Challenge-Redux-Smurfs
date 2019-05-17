d7b577b736f0e499594be04f1af8871027a7f669
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-17 10:27:15 -0700

    added addsmurf action

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,16 +4,42 @@ export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
 export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
 export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL';
 
+export const ADD_SMURF_START = 'ADD_SMURF_START';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAIL = 'ADD_SMURF_FAIL';
+
 export const getSmurfs = () => dispatch => {
-  dispatch({ type: FETCH_SMURFS_START});
+  dispatch({ type: FETCH_SMURFS_START });
   axios
     .get('http://localhost:3333/smurfs')
     .then(res =>
-      dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data })
+      dispatch({ 
+        type: FETCH_SMURFS_SUCCESS, 
+        payload: res.data })
     )
-    .catch(err => dispatch({ type: FETCH_SMURFS_FAIL, payload: err}));
+    .catch(err => 
+      dispatch({ 
+       type: FETCH_SMURFS_FAIL, 
+       payload: err
+    }));
 }
 
+export const addSmurf = (smurf) => dispatch => {
+  dispatch({ type: ADD_SMURF_START});
+  axios
+    .post('http://localhost:3333/smurfs', smurf)
+    .then(res => {
+      dispatch({
+        type: ADD_SMURF_SUCCESS,
+        payload: res.data
+      })
+    })
+    .catch(err =>
+      dispatch({
+        type: ADD_SMURF_FAIL,
+        payload: err
+      }));
+}
 
 
 
